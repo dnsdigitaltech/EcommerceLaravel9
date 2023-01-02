341c842bbc1f19c575ebd2f2929f9f2fdaa50fd2
Davi Bernardo <davi.bernardo@sbahq.org>
2023-01-02 14:59:05 -0300

    Criando o Seed para usuários

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB:table('users')->DB::insert([
+            //Admin
+            [
+                'name' => 'Admin',
+                'username' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('12345678'),
+                'role' => 'admin',
+                'status' => 'ativo',
+            ],
+            //Vendor
+            [
+                'name' => 'Bernardo Vendor',
+                'username' => 'vendor',
+                'email' => 'vendor@gmail.com',
+                'password' => Hash::make('12345678'),
+                'role' => 'vendor',
+                'status' => 'ativo',
+            ],
+            //User OR Customer
+            [
+                'name' => 'User',
+                'username' => 'user',
+                'email' => 'user@gmail.com',
+                'password' => Hash::make('12345678'),
+                'role' => 'user',
+                'status' => 'ativo',
+            ],
+        ]);
+    }
+}
