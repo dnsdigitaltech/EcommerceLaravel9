760da73f9e4ae021854c94fb50e5177994e73416
Davi Bernardo <davi.bernardo@sbahq.org>
2023-01-03 17:54:21 -0300

    Perfil e imagem do vendor parte 1

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class VendorController extends Controller
 {
@@ -15,6 +16,36 @@ class VendorController extends Controller
     public function VendorLogin()
     {
         return view('vendor.vendor_login');
+    } //End Method
+
+    public function VendorProfile()
+    {
+        $id = Auth::user()->id;
+        $vendorData = User::find($id);
+        return view('vendor.vendor_profile_view',['vendorData' => $vendorData]);
+    } //End Method
+
+    public function VendorProfileStore(Request $request)
+    {
+        $id = Auth::user()->id;
+        $data = User::find($id );
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+        if ($request->file('photo')) {
+            $file = $request->file('photo');
+            @unlink(public_path('upload/vendor_images/'.$data->photo));
+            $filename = \Str::slug(date('YmHi').$request->name).'.'.$file->getClientOriginalExtension();
+            $file->move(public_path('upload/vendor_images'), $filename);
+            $data['photo'] = $filename;
+        }
+        $data->save();
+        $notification = array(
+            'message'       => 'Perfil atualizado com sucesso.',
+            'alert-type'    => 'success'
+        );
+        return redirect()->back()->with($notification);
     } //End Method
 
     public function VendorDestroy(Request $request)
--- routes/web.php
+++ routes/web.php
@@ -43,6 +43,8 @@ Route::middleware(['auth', 'role:admin'])->group(function(){
 Route::middleware(['auth', 'role:vendor'])->group(function(){
     Route::get('/vendor/dashboard', [VendorController::class, 'VendorDashboard'])->name('vendor.dashboard');
     Route::get('/vendor/logout', [VendorController::class, 'VendorDestroy'])->name('vendor.logout');
+    Route::get('/vendor/perfil', [VendorController::class, 'VendorProfile'])->name('vendor.profile');
+    Route::post('/vendor/perfil/atualiza', [VendorController::class, 'VendorProfileStore'])->name('vendor.profile.store');
 });
 
 Route::get('/admin/login', [AdminController::class, 'AdminLogin']);
