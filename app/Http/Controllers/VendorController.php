b50f6c1ed6ef34f1da323da5b127556b13303940
Davi Bernardo <davi.bernardo@sbahq.org>
2023-01-03 17:21:02 -0300

    Logando e deslogando como vendor (vendedor)

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 
 class VendorController extends Controller
@@ -9,5 +10,21 @@ class VendorController extends Controller
     public function VendorDashboard()
     {
         return view('vendor.index');
+    } //End Method
+
+    public function VendorLogin()
+    {
+        return view('vendor.vendor_login');
+    } //End Method
+
+    public function VendorDestroy(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/vendor/login');
     } //End Method
 }
